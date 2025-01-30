f270cd0dd4900df86fcf2bdc4d9a24b8e089af46
izzokewl <gozezekiel06@gmail.com>
2025-01-30 18:39:46 +0800

    first commit

---
+++ log_system.py
@@ -0,0 +1,11 @@
+# 1st program: log system
+# ask for full name
+# first login attempt is for attendance punch in
+# second login attempt is for lunch break punch out
+# third login attempt is for lunch break punch in
+# fourth attempt is for attendance logout
+# program will store multiple entries
+
+# 2nd program: attendance list system
+# will print the list of entries that are log in attendance in, on lunch or punched out
+# the list will also be updated if the user logs everytime in program 1
