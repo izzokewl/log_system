42f65846391f2f9289b26f47ce1fcd1e8d5e53b6
izzokewl <gozezekiel06@gmail.com>
2025-01-31 17:59:40 +0800

    ask user for name

--- log_system.py
+++ log_system.py
@@ -1,13 +1,9 @@
-# 1st program: log system
-# ask for full name
-# first login attempt is for attendance punch in
-# second login attempt is for lunch break punch out
-# third login attempt is for lunch break punch in
-# fourth attempt is for attendance logout
-# program will store multiple entries
+while True:
+    try:
+        nameinput = input("Enter full name: ").title().strip()
+        if any(char.isdigit() for char in nameinput):
+          raise ValueError("\nInvalid input, try again\n")
+        break
+    except ValueError as error:
+       print(error)
 
-# 2nd program: attendance list system
-# will print the list of entries that are log in attendance in, on lunch or punched out
-# the list will also be updated if the user logs everytime in program 1
-
-#pseudocodes muna sir hehe
