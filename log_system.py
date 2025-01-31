47d0c98d39a15b58db2cd1e7326c6073fe04b63c
izzokewl <gozezekiel06@gmail.com>
2025-01-31 18:33:49 +0800

    program 1 improved

--- log_system.py
+++ log_system.py
@@ -1,9 +1,29 @@
+import datetime
+
+attendance = {}
+
+print("\n=======LOG ATTENDANCE=======\n")
 while True:
     try:
         nameinput = input("Enter full name: ").title().strip()
         if any(char.isdigit() for char in nameinput):
-          raise ValueError("\nInvalid input, try again\n")
+            raise ValueError("\nInvalid input, try again\n")
         break
     except ValueError as error:
-       print(error)
+        print(error)
 
+if nameinput not in attendance:
+    attendance[nameinput] = {}
+
+log_entries = ["Log in", "Out for lunch", "Back from lunch", "Log out"]
+attempt = len(attendance[nameinput])
+
+if attempt < len(log_entries):
+    log_time = datetime.datetime.now().strftime("%H:%M")
+
+    attendance[nameinput][log_entries[attempt]] = log_time
+    
+    print(f"{nameinput} punched for {log_entries[attempt]}")
+    print(f"Time punched: {log_time}\n")
+else:
+    print(f"{nameinput} has already completed punches for the day\n")
